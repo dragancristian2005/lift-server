7470e2333dccfe4717c193fb88aeb822fec8f33b
Cristian Dragan <cristiandragan34@yahoo.ro>
2025-01-16 13:45:33 +0200

    Implemented weekly-progress for authenticated user

--- src/workouts/workouts.controller.ts
+++ src/workouts/workouts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Request } from '@nestjs/common';
 import { WorkoutsService } from './workouts.service';
 
 @Controller('workouts')
@@ -10,13 +10,13 @@ export class WorkoutsController {
     return this.workoutsService.getAllWorkouts(Number(page), Number(limit));
   }
 
+  @Get('weekly-progress')
+  getWeeklyProgress(@Request() req) {
+    return this.workoutsService.getWeeklyProgress(req.user.sub);
+  }
+
   @Get(':id')
   getOneWorkout(@Param('id') workoutId: string) {
     return this.workoutsService.getOneWorkout(workoutId);
-  }
-
-  @Get(':userId/weekly-progress')
-  getWeeklyProgress(@Param('userId') userId: string) {
-    return this.workoutsService.getWeeklyProgress(userId);
   }
 }
